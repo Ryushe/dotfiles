48c84d2c9cb746dbb8dda89af57c3344335d1732
LilPeenieWeenie <jaadynhumphries@gmail.com>
2023-11-01 01:28:45 -0400

    can move .bashrc and .tmux.conf now

--- installeverythingforvim.sh
+++ installeverythingforvim.sh
@@ -1,51 +1,33 @@
 #!/bin/bash
 
-
 # Check if Tmux is installed
 echo "checking if tmux is installed"
 sleep 2
 
-if  command -v tmux &> /dev/null; then
-    echo "Tmux is installed"
+if command -v tmux &>/dev/null; then
+	echo "Tmux is installed"
 else
-    echo "Tmux not found\nInstallingtmux now"
-    sleep 2
-    sudo apt install tmux	
+	echo "Tmux not found\nInstallingtmux now"
+	sleep 2
+	sudo apt install tmux
 fi
-
 
 # Check if Tpm is already installed
 echo "checking if Tpm is installed"
 sleep 2
 if [ -d "$HOME/.tmux/plugins/tpm" ]; then
-    echo "Tpm is already installed."
+	echo "Tpm is already installed."
 else
 	# Install Tpm
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo "Tpm has been installed."
+	echo "Tpm has been installed."
 fi
 
-
-# Check if ~/.config/nvim exists
-echo "checking if ~/.config/nvim exists is installed"
+echo "We should do something about the .bashrc"
+echo "Moving the .bashrc to the home dir"
 sleep 2
-nvim_dir=~/.config/nvim
-if [ ! -d "$nvim_dir" ]; then
-    echo "Directory $nvim_dir does not exist. Creating it..."
-    mkdir -p "$nvim_dir"
-else
-    echo "Directory $nvim_dir already exists."
-fi
-# put the path for init.vim into default loacation (it will overwrite it)
-echo 'luafile $NVIM_INIT_VIM' > ~/.config/nvim/init.vim
+mv -f .bashrc ~
 
-# check if packer exists
-if [-d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"]; then
-    echo "packer for nvim already installed"
-else
-    # install packer
-    echo "Installing packer for Nvim"
-    sleep 2
-    git clone --depth 1 "https://github.com/wbthomason/packer.nvim" "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
-    echo "packer is now installed"
-fi
+echo "Im going to move .tmux.conf now"
+sleep 2
+mv -f .tmux.conf ~
