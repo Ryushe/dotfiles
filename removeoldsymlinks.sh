11903348767e0a490a6e3c8609fe7fafb1c63f81
LilPeenieWeenie <jaadynhumphries@gmail.com>
2024-08-09 16:54:57 -0400

    kde vm shortcuts

--- removeoldsymlinks.sh
+++ removeoldsymlinks.sh
@@ -1,8 +1,9 @@
 files=( ".bashrc" ".vimrc" ".tmux.conf" ".config/kitty/kitty.conf" ".config/kglobalshortcutsrc" ".config/khotkeysrc" )
 
-for file in files
+for file in "${files[@]}"
 do 
   rm $HOME/$file
   echo removed $file
+done
 
 
