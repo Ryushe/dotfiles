20d8ba93486183cac13f376de161bf177dc96d20
Ryushe <ryushe.dev@gmail.com>
2024-09-06 14:57:22 -0700

    hyprland and minor changes

--- scripts/swap_active_workspaces.sh
+++ scripts/swap_active_workspaces.sh
@@ -6,13 +6,13 @@ current_mon_name=$(hyprctl monitors | grep -A 1 "ID $current_mon_id" | grep "Mon
 
 declare -A monitor_moves
 #left  right movements
-monitor_moves["$left_mon"]="$main_mon $right_mon"
+monitor_moves["$left_mon"]="$right_mon $main_mon"
 monitor_moves["$main_mon"]="$left_mon $right_mon"
 monitor_moves["$right_mon"]="$main_mon $left_mon"
 
 possible_moves=${monitor_moves[$current_mon_name]}
 
-if [ $1 = "l" ]; then
+if [[ $1 = "l" ]]; then
   new_mon=$(echo "$possible_moves" | awk '{print $1}')
 elif [[ $1 = "r" ]]; then
   new_mon=$(echo "$possible_moves" | awk '{print $2}')
