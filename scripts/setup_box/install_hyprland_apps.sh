2e578aa5b048975fb5a0cea3d65aec6c5ef06df8
Ryushe <ryushe.dev@gmail.com>
2025-06-03 19:13:50 -0700

    more cleanup

--- scripts/setup_box/install_hyprland_apps.sh
+++ scripts/setup_box/install_hyprland_apps.sh
@@ -30,10 +30,14 @@ apps=(
   "fcitx5-mozc"
   "fcitx5-qt"
   "gammastep"
+  "antimicrox"
 )
 sudo pacman -S "${apps[@]}"
 
-yay_apps=("hyprsome")
+yay_apps=("hyprsome" "hyprtheme")
 for app in "${yay_apps[@]}"; do
   yay -S $app
 done
+
+echo "if still using ax-shell install it with"
+echo "curl -fsSL https://raw.githubusercontent.com/Axenide/Ax-Shell/main/install.sh | bash"
